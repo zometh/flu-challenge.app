22b6cddb60e35f98b06978ad69c964a319cf0e11
Mouhamed Diop <metzodiop45@gmail.com>
2025-04-07 20:59:11 +0000

    style: ajouter le fichier color.dart pour la configuration des couleurs globales

---
+++ ecom/lib/config/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kprimary = Color(0xFFFFDD00);
+const Color ksecondary = Color(0xFFFF8400);
+const Color ktertiary = Color(0xFFF9F9F9);
+const Color kgrey = Color(0xFFCDCDCD);
+const Color kDarkColor = Colors.black;
