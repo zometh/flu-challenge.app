dc1c89e46ddddf6afb6e2f98ed3f6483bea31ef9
Mouhamed Diop <metzodiop45@gmail.com>
2025-04-07 23:08:47 +0000

    Update product_view_controller.dart

--- ecom/lib/controllers/product_view_controller.dart
+++ ecom/lib/controllers/product_view_controller.dart
@@ -1,10 +1,24 @@
+import 'package:carousel_slider/carousel_controller.dart';
 import 'package:get/get.dart';
 
 class ProductViewController extends GetxController {
   RxInt currentImage = 0.obs;
-
+  RxInt currentProductChoice = 0.obs;
+  RxBool isFavorite = false.obs;
+  Rx<CarouselSliderController> sliderController =
+      CarouselSliderController().obs;
   upadeImageIndex(int newIndex) {
     currentImage.value = newIndex;
+    update();
+  }
+
+  upadeChoiceIndex(int newIndex) {
+    currentProductChoice.value = newIndex;
+    update();
+  }
+
+  updateFavorite() {
+    isFavorite.value = !isFavorite.value;
     update();
   }
 }
